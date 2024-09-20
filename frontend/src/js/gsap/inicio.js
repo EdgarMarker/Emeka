726654346d9d4f117493904fffeea0dc9478f7ec
Miguel Espinosa <miguel@Mac-mini-de-Miguel.local>
2024-09-20 00:21:04 -0600

    hero inicio

--- frontend/src/js/gsap/inicio.js
+++ frontend/src/js/gsap/inicio.js
@@ -1,5 +1,5 @@
 // Btn ScrollTo:
-const heroBtn = document.querySelector('#section__hero .btn');
+const heroBtn = document.querySelector('#section__hero .btn__hero');
 heroBtn.addEventListener('click', () => {
     gsap.to(window, { duration: 0.5, scrollTo: "#section__intro", ease: "power4.out"});
 });
@@ -31,17 +31,57 @@ function gsapSoloAnimations() {
         duration: 0.5,
         ease: "power1.out"
     }, "-=0.2")
-    .from("#section__hero .btn", {
-        opacity: 0,
-        x: -50,
-        duration: 0.5,
-        ease: "power1.out"
-    }, "-=0.2")
     .from("#section__hero picture", {
         opacity: 0,
         y: -50,
         duration: 0.8,
         ease: "power1.out"
     }, "-=0.8")
+    
+    // Animacion giro heroBtn:
+
+    gsap.to(".btn__circle", {
+        rotation:360*2,
+        duration:1,
+        ease:'none',
+        scrollTrigger: {
+            trigger: "#frase__hero",
+            start:"top 50%",
+            end: "bottom 50%",
+            scrub: true,
+        },
+    });
+
+    // Hero
+
+    gsap.to(".img__hero", {
+        width: "50%",
+        transformOrigin: "center",
+        ease: "power4.out",
+        scrollTrigger: {
+            trigger: ".img__hero",
+            start: "top 60",
+            end: "+=160%",
+            endTrigger: ".column__3",
+            scrub: true,
+            pin: ".img__hero",
+            pinSpacing: false,
+        }
+    })
+
+    gsap.to("#video__inicio", {
+        clipPath: "circle(100% at 50% 50%)",
+        ease: "power1.in",
+        scrollTrigger: {
+            trigger: ".vid__hero",
+            start: "top 0%",
+            end: "bottom 100%",
+            endTrigger: ".vid__hero",
+            scrub: true,
+            pin: "#video__inicio",
+            pinSpacing: false,
+            markers: true,
+        }
+    })
 
 }
